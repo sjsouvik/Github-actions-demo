f9cdb817cf36cb0934bfeff2cf6a5dcbb2a38aba
Souvik Jana <souvikjana2013@gmail.com>
2023-04-30 22:38:02 +0530

    refactor: update app heading

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { ChatList } from "./components/ChatList/ChatList";
 export default function App() {
   return (
     <div className="App">
-      <h1>Flipkart Chat Support</h1>
+      <h1>My Chat Support</h1>
       <ChatList />
     </div>
   );
